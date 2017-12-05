a246bdfa62aa68ab7964958d2bb19b14bd37a72c
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2017-12-05 19:04:41 +0000

    First commit...teste

---
+++ src/main/webapp/plugins/cronapp-framework-js/js/controllers.js
@@ -0,0 +1,23 @@
+(function($app) {
+  angular.module('custom.controllers', []);
+
+  app.controller('HomeController', [ '$scope', '$http', '$rootScope', '$state', '$translate', 'Notification', 'ReportService',
+      function($scope, $http, $rootScope, $state, $translate, Notification, ReportService) {
+
+        $rootScope.http = $http;
+        $rootScope.Notification = Notification;
+
+
+        $rootScope.getReport = function(reportName, params) {
+          ReportService.openReport(reportName, params);
+        }
+        
+        app.registerEventsCronapi($scope, $translate);
+
+        for( var x in app.userEvents)
+          $scope[x] = app.userEvents[x].bind($scope);
+
+        $scope.message = {};
+
+      } ]);
+}(app));
